ed12923651c2e3cdb82c89a85407c6ee6b0dea09
Matteo Concas <matteo.concas@cern.ch>
2021-02-27 13:00:23 +0100

    Obtain list of samples

--- main.cxx
+++ main.cxx
@@ -9,29 +9,44 @@ int main(int argc, char *argv[])
 {
     if (argv[1] == nullptr)
     {
-        std::cerr << "Please provide input file" << std::endl;
+        cerr << "Please provide input file" << endl;
         return 1;
     }
 
-    // std::string filename = argv[1];
-    // ifstream datafile(filename);
-    // std::cout << "Reading file: " << filename << std::endl;
+    string filename = argv[1];
+    ifstream datafile(filename);
+    cout << "Reading file: " << filename << endl;
 
-    // int row{0};
-    // while (!datafile.eof())
-    // {
-    //     if (!row)
-    //     {
-    //         std::string str;
-    //         std::getline(datafile, str);
-    //         std::stringstream ss(str);
-    //         int col = 0;
-    //         // while (ss >> my_array[row][col])
-    //         // {
-    //         //     col++;
-    //         // }
-    //         row++;
-    //     }
-    // }
+    int row{0};
+    vector<int> tmpVec(N, -1);
+
+    string line;
+    vector<sample> samplesVector;
+    sample s{""}; // temporary sample
+    while (getline(datafile, line))
+    {
+        if (row == 0)
+        {
+            line = ""; // skip first header (ugly)
+        }
+        else
+        {
+            stringstream iss(line);
+            if (!s.parseStream(iss))
+            {
+                samplesVector.emplace_back(s);
+                stringstream iss2(line);
+                s.reset();
+                s.parseStream(iss2);
+            }
+        }
+        row++;
+    }
+    samplesVector.emplace_back(s); // push last sample
+    cout << "Found " << samplesVector.size() << " samples" << endl;
+    for (auto &sampleElem : samplesVector)
+    {
+        sampleElem.print();
+    }
     return 0;
 }
--- sample.h
+++ sample.h
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <vector>
 #include <utility>
 #include <unordered_map>
+#include <sstream>
 
 #define N 12
+// #define DEBUG 1
 
 using namespace std;
 
@@ -12,13 +15,17 @@ struct sample
 {
     sample(string name) : mName(name) { reset(); }
     void reset();
-    void setMarkerValues(string marker, std::vector<int> values); // set marker values
-    bool operator==(sample &rhs);                                 // comparison operator
+    void setMarkerValues(string marker, vector<int> values); // set marker values
+    bool operator==(sample &rhs);                            // comparison operator
+    void print();                                            // debug print
+    bool parseStream(stringstream &stream);                  // digest data from stream and set map accordingly
+    vector<int> &getMarkerValues(string marker);             // get reference to a given a certain label
 
+    string mSampleFile = "";                        // Sample File
     string mName;                                   // Name
     unordered_map<string, vector<int>> mMarkersMap; // Map of markers values
-    vector<int> &getMarkerValues(string marker);    // get reference to a given a certain label
-    bool mUsed = false;                             // is used?
+
+    bool mUsed = false; // is used?
 };
 
 inline bool sample::operator==(sample &rhs)
@@ -50,18 +57,21 @@ inline vector<int> &sample::getMarkerValues(string marker)
     }
     else
     {
-        cerr << "Marker name not found" << std::endl;
+        cerr << "Marker name not found" << endl;
         throw;
     }
 }
 
-void sample::setMarkerValues(string marker, std::vector<int> values) // set marker values
+inline void sample::setMarkerValues(string marker, vector<int> values) // set marker values
 {
     getMarkerValues(marker) = values;
 }
 
-void sample::reset()
+inline void sample::reset()
 {
+    mSampleFile = "";
+    mName = "";
+
     mMarkersMap.insert(make_pair<string, vector<int>>("ANUCS501", vector<int>(N, -1)));
     mMarkersMap.insert(make_pair<string, vector<int>>("9269", vector<int>(N, -1)));
     mMarkersMap.insert(make_pair<string, vector<int>>("4910", vector<int>(N, -1)));
@@ -77,4 +87,111 @@ void sample::reset()
     mMarkersMap.insert(make_pair<string, vector<int>>("H06", vector<int>(N, -1)));
 
     mUsed = false;
-};
+}
+
+inline void sample::print()
+{
+    cout << "Sample File: " << mSampleFile << endl;
+    cout << "Sample Name: " << mName << endl;
+    cout << "Markers: " << endl;
+    for (auto &marker : mMarkersMap)
+    {
+        cout << " • " << setw(10) << marker.first;
+        for (auto &value : marker.second)
+        {
+            if (value != -2)
+            {
+                cout << "\t" << value;
+            }
+            else
+            {
+                cout << "\t - ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+inline bool sample::parseStream(stringstream &stream)
+{
+    string tmpFileName, tmpSampleName;
+    stream >> tmpFileName >> tmpSampleName;
+    if (mSampleFile == "" && mName == "")
+    {
+        // This is a new uninitialized sample object!
+        mSampleFile = tmpFileName;
+        mName = tmpSampleName;
+
+        // Get subsequent data
+        string tmpMarker;
+        string tmpdye;
+        vector<float> tmpValues(3 * N, -2);
+        stream >> tmpMarker >> tmpdye;
+        for (int iVal{0}; iVal < 3 * N; ++iVal)
+        {
+            stream >> tmpValues[iVal];
+        }
+
+        // Find marker:
+        auto it = mMarkersMap.find(tmpMarker);
+        if (it != mMarkersMap.end())
+        {
+            // Set values (every three tmpVals)
+            for (int iVal{0}; iVal < N; ++iVal)
+            {
+                (it->second)[iVal] = (int)tmpValues[3 * iVal];
+            }
+        }
+        else
+        {
+            cerr << "Marker name: " << tmpMarker << " not found" << endl;
+            throw;
+        }
+
+        return true;
+    }
+    else
+    {
+        // This sample has already been initialized
+        if (mSampleFile != tmpFileName || mName != tmpSampleName)
+        {
+#ifdef DEBUG
+            cerr << "Stream data is incompatible with this sample!" << endl;
+            cerr << "Filename: " << tmpFileName << " Samplename: " << tmpSampleName << endl;
+            cerr << "Sample dump follows:" << endl;
+            print();
+#endif
+            return false;
+        }
+        else
+        {
+            // Get subsequent data
+            string tmpMarker;
+            string tmpdye;
+            vector<float> tmpValues(3 * N, -2);
+            stream >> tmpMarker >> tmpdye;
+            for (int iVal{0}; iVal < 3 * N; ++iVal)
+            {
+                stream >> tmpValues[iVal];
+            }
+
+            // Find marker:
+            auto it = mMarkersMap.find(tmpMarker);
+            if (it != mMarkersMap.end())
+            {
+                // Set values (every three tmpVals)
+                for (int iVal{0}; iVal < N; ++iVal)
+                {
+                    (it->second)[iVal] = (int)tmpValues[3 * iVal];
+                }
+            }
+            else
+            {
+                cerr << "Marker name: " << tmpMarker << " not found" << endl;
+                throw;
+            }
+
+            return true;
+        }
+    }
+}
