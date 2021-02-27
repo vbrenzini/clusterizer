451921f026d098d3fc65df390aa006de8b25cf00
Matteo Concas <matteo.concas@cern.ch>
2021-02-27 14:39:38 +0100

    Add output and makefile for macos

--- main.cxx
+++ main.cxx
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
 #include "sample.h"
 
 using namespace std;
@@ -43,10 +44,48 @@ int main(int argc, char *argv[])
         row++;
     }
     samplesVector.emplace_back(s); // push last sample
+#ifdef DEBUG
     cout << "Found " << samplesVector.size() << " samples" << endl;
     for (auto &sampleElem : samplesVector)
     {
         sampleElem.print();
+    }
+#endif
+    // Actual clusterization
+    vector<vector<sample>> sampleClusters;
+    for (int iSample{0}; iSample < samplesVector.size(); ++iSample)
+    {
+        if (samplesVector[iSample].mUsed)
+        {
+            continue;
+        }
+        sampleClusters.push_back(vector<sample>{samplesVector[iSample]});
+        samplesVector[iSample].mUsed = true;
+        for (int jSample{0}; jSample < samplesVector.size(); ++jSample)
+        {
+            if ((iSample != jSample) && !samplesVector[jSample].mUsed)
+            {
+                if (samplesVector[iSample] == samplesVector[jSample])
+                {
+                    sampleClusters.back().push_back(samplesVector[jSample]);
+                    samplesVector[jSample].mUsed = true;
+                }
+            }
+        }
+    }
+    sort(sampleClusters.begin(), sampleClusters.end(), [](auto &i, auto &j) { return i.size() > j.size(); });
+
+    // print results
+    int counter{0};
+    for (auto &cluster : sampleClusters)
+    {
+        cout << "Cluster " << counter << ": ";
+        for (auto &s : cluster)
+        {
+            cout << s.mName << "\t";
+        }
+        ++counter;
+        cout << endl;
     }
     return 0;
 }
